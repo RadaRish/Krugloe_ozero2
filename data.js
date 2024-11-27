7f927c36ca1dddbebb3c48315e3c11881c517e22
RadaRish <107124341+RadaRish@users.noreply.github.com>
2024-11-27 18:38:26 +0300

    Upload

---
+++ data.js
@@ -0,0 +1,239 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "Парковка",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2736558273382332,
+          "pitch": 0.04419030841558502,
+          "rotation": 0,
+          "target": "1-_1_"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-_1_",
+      "name": "Глэмпинг_1_веранда",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9883679332701139,
+          "pitch": 0.06891122499975744,
+          "rotation": 0,
+          "target": "2-_2_"
+        },
+        {
+          "yaw": 3.0942150197145537,
+          "pitch": 0.08660192921406917,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-_2_",
+      "name": "Глэмпинг_2_веранда",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6732937543727306,
+          "pitch": 0.07308724542138201,
+          "rotation": 0,
+          "target": "1-_1_"
+        },
+        {
+          "yaw": 1.5751859012995721,
+          "pitch": -0.005876279500782289,
+          "rotation": 0,
+          "target": "3-_3_"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-_3_",
+      "name": "Глэмпинг_3_веранда",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.247791523563535,
+          "pitch": 0.03397938795244215,
+          "rotation": 0,
+          "target": "2-_2_"
+        },
+        {
+          "yaw": -1.6518647317318624,
+          "pitch": 0.20079510126385713,
+          "rotation": 0,
+          "target": "4-_3_"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-_3_",
+      "name": "Глэмпинг_3_внутри",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.34601972721493723,
+          "pitch": 0.1629885947794314,
+          "rotation": 0,
+          "target": "3-_3_"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
